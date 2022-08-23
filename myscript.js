1fe2f5a2b904952b0af23034ec6171ce48446095
Luz42 <a.evangelista93@gmail.com>
2022-08-23 16:56:16 +0200

    add .toFixed to limit decimals to results

--- myscript.js
+++ myscript.js
@@ -9,17 +9,20 @@ const age = prompt('Inserire l\'età del passeggero');
 const full_price = distance * 0.21;
 
 if (age >= 18 && age < 65){
-    document.writeln('Costo biglietto ' + full_price);    
+    const final_price = full_price.toFixed(2)
+    document.writeln('Il costo del biglietto è ' + final_price);    
 }
 
 else if (age < 18){
     minor_price = (full_price * 20) / 100
-    document.writeln('Costo biglietto ' + minor_price)
+    const final_price = minor_price.toFixed(2)    
+    document.writeln('Il costo del biglietto è ' + final_price)
 }
 
 else if (age >= 65){
     mayor_price = (full_price * 40) / 100
-    document.writeln('Costo biglietto ' + mayor_price)
+    const final_price = mayor_price.toFixed(2)
+    document.writeln('Il costo del biglietto è ' + final_price)
 }
 
 
