d21429a4bad73dff01f9f32c321ff262bf5b1a62
Luz42 <a.evangelista93@gmail.com>
2022-08-23 16:13:58 +0200

    add const for distance and age add const who calc the full_price add condition for discount based on the age of passenger

--- myscript.js
+++ myscript.js
@@ -1,1 +1,25 @@
-alert('Hello World');
+//document.writeln('Hello World');
+
+const distance = prompt('Inserire la distanza da percorrere, in Km');
+const age = prompt('Inserire l\'età del passeggero');
+
+//const somma = parseInt(distance) + parseInt(age);
+//document.writeln(somma);
+
+const full_price = distance * 0.21;
+
+if (age >= 18 && age < 65){
+    document.writeln('Costo biglietto ' + full_price);    
+}
+
+else if (age < 18){
+    minor_price = (full_price * 20) / 100
+    document.writeln('Costo biglietto ' + minor_price)
+}
+
+else if (age >= 65){
+    mayor_price = (full_price * 40) / 100
+    document.writeln('Costo biglietto ' + mayor_price)
+}
+
+
