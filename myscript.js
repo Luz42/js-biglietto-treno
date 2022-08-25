545f8943820b2ce7d7c346e1113a69fd636dbccb
Luz42 <a.evangelista93@gmail.com>
2022-08-25 16:51:41 +0200

    fixed discount calc

--- myscript.js
+++ myscript.js
@@ -14,13 +14,13 @@ if (age >= 18 && age < 65){
 }
 
 else if (age < 18){
-    minor_price = (full_price * 20) / 100
+    minor_price = (full_price - ((full_price * 20) / 100))
     const final_price = minor_price.toFixed(2)    
     document.writeln('Il costo del biglietto è ' + final_price)
 }
 
 else if (age >= 65){
-    mayor_price = (full_price * 40) / 100
+    mayor_price = (full_price - ((full_price * 40) / 100))
     const final_price = mayor_price.toFixed(2)
     document.writeln('Il costo del biglietto è ' + final_price)
 }
